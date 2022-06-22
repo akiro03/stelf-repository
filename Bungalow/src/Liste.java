3a757c9fa29f16888c028747f37beca9b35d6ed7
Ersin Güngör <Ersin Gngr@Ersin-Pc>
2022-06-22 19:34:35 +0200

    Update 22.06.2022

---
+++ Bungalow/src/Liste.java
@@ -0,0 +1,188 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+@SuppressWarnings("serial")
+public class Liste extends JFrame {
+
+	private JFrame frmListe;
+	private JTextField txtField1;
+	private JTextField txtField2;
+	private JTextField txtField3;
+	private JTextField txtField4;
+	private JTextField txtField5;
+	private JTextField txtField6;
+	private JTextField txtField7;
+	private JTextField txtField8;
+	private JTextField txtField9;
+	private JTextField txtField10;
+
+	// ArrayList<Integer> bungPersonen = new ArrayList<>();
+	int[] bungPersonen = new int[10];
+	Random random = new Random();
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Liste window = new Liste();
+					window.frmListe.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	 Liste() {
+		bungalowPersonen();
+		frmListe = new JFrame();
+		frmListe.setTitle("Liste");
+		frmListe.setForeground(Color.WHITE);
+		frmListe.setBackground(Color.DARK_GRAY);
+		frmListe.setAlwaysOnTop(true);
+		frmListe.setBounds(100, 100, 338, 444);
+		frmListe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JPanel contentPane = new JPanel();
+		contentPane.setLayout(null);
+		contentPane.setForeground(Color.BLACK);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(new Color(51, 51, 51));
+		frmListe.getContentPane().add(contentPane, BorderLayout.CENTER);
+
+		JLabel lblBungalow_1 = new JLabel("Bungalow 1:");
+		lblBungalow_1.setForeground(Color.WHITE);
+		lblBungalow_1.setBackground(Color.WHITE);
+		lblBungalow_1.setBounds(27, 35, 76, 23);
+		contentPane.add(lblBungalow_1);
+
+		JLabel lblBungalow_2 = new JLabel("Bungalow 2:");
+		lblBungalow_2.setForeground(Color.WHITE);
+		lblBungalow_2.setBackground(Color.WHITE);
+		lblBungalow_2.setBounds(27, 69, 76, 23);
+		contentPane.add(lblBungalow_2);
+
+		JLabel lblBungalow_3 = new JLabel("Bungalow 3:");
+		lblBungalow_3.setForeground(Color.WHITE);
+		lblBungalow_3.setBackground(Color.WHITE);
+		lblBungalow_3.setBounds(27, 103, 76, 23);
+		contentPane.add(lblBungalow_3);
+
+		JLabel lblBungalow_4 = new JLabel("Bungalow 4:");
+		lblBungalow_4.setForeground(Color.WHITE);
+		lblBungalow_4.setBackground(Color.WHITE);
+		lblBungalow_4.setBounds(27, 137, 76, 23);
+		contentPane.add(lblBungalow_4);
+
+		JLabel lblBungalow_5 = new JLabel("Bungalow 5:");
+		lblBungalow_5.setForeground(Color.WHITE);
+		lblBungalow_5.setBackground(Color.WHITE);
+		lblBungalow_5.setBounds(27, 171, 76, 23);
+		contentPane.add(lblBungalow_5);
+
+		JLabel lblBungalow_6 = new JLabel("Bungalow 6:");
+		lblBungalow_6.setForeground(Color.WHITE);
+		lblBungalow_6.setBackground(Color.WHITE);
+		lblBungalow_6.setBounds(27, 205, 76, 23);
+		contentPane.add(lblBungalow_6);
+
+		JLabel lblBungalow_7 = new JLabel("Bungalow 7:");
+		lblBungalow_7.setForeground(Color.WHITE);
+		lblBungalow_7.setBackground(Color.WHITE);
+		lblBungalow_7.setBounds(27, 239, 76, 23);
+		contentPane.add(lblBungalow_7);
+
+		JLabel lblBungalow_8 = new JLabel("Bungalow 8:");
+		lblBungalow_8.setForeground(Color.WHITE);
+		lblBungalow_8.setBackground(Color.WHITE);
+		lblBungalow_8.setBounds(27, 273, 76, 23);
+		contentPane.add(lblBungalow_8);
+
+		JLabel lblBungalow_9 = new JLabel("Bungalow 9:");
+		lblBungalow_9.setForeground(Color.WHITE);
+		lblBungalow_9.setBackground(Color.WHITE);
+		lblBungalow_9.setBounds(27, 307, 76, 23);
+		contentPane.add(lblBungalow_9);
+
+		JLabel lblBungalow_10 = new JLabel("Bungalow 10:");
+		lblBungalow_10.setForeground(Color.WHITE);
+		lblBungalow_10.setBackground(Color.WHITE);
+		lblBungalow_10.setBounds(27, 341, 76, 23);
+		contentPane.add(lblBungalow_10);
+
+		txtField1 = new JTextField("Max. " + bungPersonen[0] + " Personen");
+		txtField1.setEditable(false);
+		txtField1.setBounds(113, 38, 154, 20);
+		contentPane.add(txtField1);
+		txtField1.setColumns(10);
+
+		txtField2 = new JTextField("Max. " + bungPersonen[1] + " Personen");
+		txtField2.setEditable(false);
+		txtField2.setColumns(10);
+		txtField2.setBounds(113, 70, 154, 20);
+		contentPane.add(txtField2);
+
+		txtField3 = new JTextField("Max. " + bungPersonen[2] + " Personen");
+		txtField3.setEditable(false);
+		txtField3.setColumns(10);
+		txtField3.setBounds(113, 104, 154, 20);
+		contentPane.add(txtField3);
+
+		txtField4 = new JTextField("Max. " + bungPersonen[3] + " Personen");
+		txtField4.setEditable(false);
+		txtField4.setColumns(10);
+		txtField4.setBounds(113, 138, 154, 20);
+		contentPane.add(txtField4);
+
+		txtField5 = new JTextField("Max. " + bungPersonen[4] + " Personen");
+		txtField5.setEditable(false);
+		txtField5.setColumns(10);
+		txtField5.setBounds(113, 172, 154, 20);
+		contentPane.add(txtField5);
+
+		txtField6 = new JTextField("Max. " + bungPersonen[5] + " Personen");
+		txtField6.setEditable(false);
+		txtField6.setColumns(10);
+		txtField6.setBounds(113, 206, 154, 20);
+		contentPane.add(txtField6);
+
+		txtField7 = new JTextField("Max. " + bungPersonen[6] + " Personen");
+		txtField7.setEditable(false);
+		txtField7.setColumns(10);
+		txtField7.setBounds(113, 240, 154, 20);
+		contentPane.add(txtField7);
+
+		txtField8 = new JTextField("Max. " + bungPersonen[7] + " Personen");
+		txtField8.setEditable(false);
+		txtField8.setColumns(10);
+		txtField8.setBounds(113, 274, 154, 20);
+		contentPane.add(txtField8);
+
+		txtField9 = new JTextField("Max. " + bungPersonen[8] + " Personen");
+		txtField9.setEditable(false);
+		txtField9.setColumns(10);
+		txtField9.setBounds(113, 308, 154, 20);
+		contentPane.add(txtField9);
+
+		txtField10 = new JTextField("Max. " + bungPersonen[9] + " Personen");
+		txtField10.setEditable(false);
+		txtField10.setColumns(10);
+		txtField10.setBounds(113, 342, 154, 20);
+		contentPane.add(txtField10);
+	}
+
+	public void bungalowPersonen() {
+		for (int i = 0; i < 10; i++) {
+			bungPersonen[i] = random.nextInt(6) + 2;
+			System.out.println(i + 1 + ". Bungalow: \t" + bungPersonen[i]);
+		}
+	}
+}
--- Bungalow/src/bungalow.java
+++ Bungalow/src/bungalow.java
@@ -13,37 +13,40 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+
 //Test
 @SuppressWarnings("serial")
 public class bungalow extends JFrame implements ActionListener {
- 
+	
 	double zahl1 = 0, zahl2 = 0, ergebnis = 0;
- 
+
 	public static final DecimalFormat df = new DecimalFormat("0.00");
- 
+
 	public int[][] Buchungen = new int[10][4];
-	
+
 	Random random = new Random();
-	
-	public int[] bungPersonen = new int[10];
- 
-	private JPanel contentPane;
-	private JTextField txtNumberOne;
-	private JLabel lblNumberOne;
-	private JLabel lblNumberTwo;
-	private JTextField txtNumberTwo;
-	private JTextField txtNumberThree;
-	private JButton btnBuchen;
-	private JLabel lblWieVielePersonen;
-	private JTextField txtNumberFour;
-	private JLabel lblWelcherBungalowSoll;
-	private JTextField txtNumberFive;
-	private JTextField txtNumberSix;
-	private JButton btnAnzeigen;
-	private JTextField txtNumberSeven;
-	private JTextField txtNumberEight;
-	private JTextField txtNumberNine;
- 
+
+
+	 JPanel contentPane;
+	 JTextField txtNumberOne;
+	 JLabel lblNumberOne;
+	 JLabel lblNumberTwo;
+	 JTextField txtNumberTwo;
+	 JTextField txtNumberThree;
+	 JLabel lblEndeKalenderwoche;
+	 JButton btnBuchen;
+	 JLabel lblWieVielePersonen;
+	 JTextField txtNumberFour;
+	 JLabel lblWelcherBungalowSoll;
+	 JTextField txtNumberFive;
+	 JTextField txtNumberSix;
+	 JButton btnAnzeigen;
+	 JTextField txtNumberSeven;
+	 JTextField txtNumberEight;
+	 JTextField txtNumberNine;
+	 JButton btnBungalowListe;
+	 
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -56,39 +59,42 @@ public class bungalow extends JFrame implements ActionListener {
 			}
 		});
 	}
- 
-	
-	public bungalow() {
-		
-		for (int i = 0; i <= 10; i++) {
-			int j = 0;
-			bungPersonen[j] = random.nextInt(10)+1;
-			System.out.println(i + " " +  bungPersonen[j]);
-			j++;
-		}
-		
-		
- 
+
+	 bungalow() {
+
 		setBackground(new Color(0, 0, 0));
 		setForeground(new Color(255, 255, 255));
 		setTitle("Buchung");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(200, 100, 600, 350);
+		setBounds(200, 100, 790, 411);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(51, 51, 51));
 		contentPane.setForeground(Color.BLACK);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
- 
-		
+
 		lblNumberOne = new JLabel("Nummer des Bungalows(1-10):");
 		lblNumberOne.setForeground(Color.WHITE);
 		lblNumberOne.setBackground(Color.WHITE);
 		lblNumberOne.setBounds(27, 35, 180, 23);
 		contentPane.add(lblNumberOne);
 		
- 
+		lblNumberTwo = new JLabel("Start Kalenderwoche (1-51):");
+		lblNumberTwo.setForeground(Color.WHITE);
+		lblNumberTwo.setBounds(27, 69, 180, 25);
+		contentPane.add(lblNumberTwo);
+
+		lblEndeKalenderwoche = new JLabel("Ende Kalenderwoche (1-51):");
+		lblEndeKalenderwoche.setForeground(Color.WHITE);
+		lblEndeKalenderwoche.setBounds(27, 104, 180, 25);
+		contentPane.add(lblEndeKalenderwoche);
+	
+		lblWieVielePersonen = new JLabel("Wie viele Personen:");
+		lblWieVielePersonen.setForeground(Color.WHITE);
+		lblWieVielePersonen.setBounds(27, 139, 180, 25);
+		contentPane.add(lblWieVielePersonen);
+		
 		txtNumberOne = new JTextField();
 		txtNumberOne.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberOne.setForeground(new Color(255, 255, 255));
@@ -96,12 +102,7 @@ public class bungalow extends JFrame implements ActionListener {
 		txtNumberOne.setBounds(220, 36, 118, 20);
 		contentPane.add(txtNumberOne);
 		txtNumberOne.setColumns(10);
- 
-		lblNumberTwo = new JLabel("Start Kalenderwoche (1-51):");
-		lblNumberTwo.setForeground(Color.WHITE);
-		lblNumberTwo.setBounds(27, 69, 180, 25);
-		contentPane.add(lblNumberTwo);
- 
+
 		txtNumberTwo = new JTextField();
 		txtNumberTwo.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberTwo.setForeground(new Color(255, 255, 255));
@@ -109,10 +110,7 @@ public class bungalow extends JFrame implements ActionListener {
 		txtNumberTwo.setBounds(220, 71, 118, 20);
 		contentPane.add(txtNumberTwo);
 		txtNumberTwo.setColumns(10);
-		JLabel lblEndeKalenderwoche = new JLabel("Ende Kalenderwoche (1-51):");
-		lblEndeKalenderwoche.setForeground(Color.WHITE);
-		lblEndeKalenderwoche.setBounds(27, 104, 180, 25);
-		contentPane.add(lblEndeKalenderwoche);
+		
 		txtNumberThree = new JTextField();
 		txtNumberThree.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberThree.setForeground(Color.WHITE);
@@ -120,18 +118,7 @@ public class bungalow extends JFrame implements ActionListener {
 		txtNumberThree.setBackground(Color.DARK_GRAY);
 		txtNumberThree.setBounds(220, 107, 118, 20);
 		contentPane.add(txtNumberThree);
-		btnBuchen = new JButton("Buchen");
-		btnBuchen.setForeground(Color.BLACK);
-		btnBuchen.addActionListener(this);
-		btnBuchen.setBackground(Color.CYAN);
-		btnBuchen.setBounds(110, 190, 110, 25);
-		contentPane.add(btnBuchen);
- 
-		lblWieVielePersonen = new JLabel("WIe viele Personen");
-		lblWieVielePersonen.setForeground(Color.WHITE);
-		lblWieVielePersonen.setBounds(27, 139, 180, 25);
-		contentPane.add(lblWieVielePersonen);
- 
+	
 		txtNumberFour = new JTextField();
 		txtNumberFour.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberFour.setForeground(Color.WHITE);
@@ -139,21 +126,15 @@ public class bungalow extends JFrame implements ActionListener {
 		txtNumberFour.setBackground(Color.DARK_GRAY);
 		txtNumberFour.setBounds(220, 142, 118, 20);
 		contentPane.add(txtNumberFour);
- 
-		lblWelcherBungalowSoll = new JLabel("<html><body><center> Welcher Bungalow soll <br>angezeigt werden</center></body></html>");
-		lblWelcherBungalowSoll.setForeground(Color.WHITE);
-		lblWelcherBungalowSoll.setBackground(Color.WHITE);
-		lblWelcherBungalowSoll.setBounds(403, 30, 200, 30);
-		contentPane.add(lblWelcherBungalowSoll);
- 
+
 		txtNumberFive = new JTextField();
 		txtNumberFive.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberFive.setForeground(Color.WHITE);
 		txtNumberFive.setColumns(10);
 		txtNumberFive.setBackground(Color.DARK_GRAY);
-		txtNumberFive.setBounds(411, 72, 118, 20);
+		txtNumberFive.setBounds(563, 71, 190, 20);
 		contentPane.add(txtNumberFive);
- 
+
 		txtNumberSix = new JTextField();
 		txtNumberSix.setDropMode(DropMode.INSERT);
 		txtNumberSix.setEditable(false);
@@ -161,55 +142,94 @@ public class bungalow extends JFrame implements ActionListener {
 		txtNumberSix.setForeground(Color.BLACK);
 		txtNumberSix.setColumns(10);
 		txtNumberSix.setBackground(Color.LIGHT_GRAY);
-		txtNumberSix.setBounds(411, 142, 118, 20);
+		txtNumberSix.setBounds(563, 141, 190, 20);
 		contentPane.add(txtNumberSix);
- 
-		btnAnzeigen = new JButton("Anzeigen");
-		btnAnzeigen.setForeground(Color.BLACK);
-		btnAnzeigen.setBackground(Color.CYAN);
-		btnAnzeigen.setBounds(411, 106, 118, 25);
-		contentPane.add(btnAnzeigen);
-		btnAnzeigen.addActionListener(this);
- 
+
 		txtNumberSeven = new JTextField();
 		txtNumberSeven.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberSeven.setForeground(Color.BLACK);
 		txtNumberSeven.setColumns(10);
 		txtNumberSeven.setBackground(Color.LIGHT_GRAY);
-		txtNumberSeven.setBounds(411, 172, 118, 20);
+		txtNumberSeven.setBounds(563, 172, 190, 20);
 		contentPane.add(txtNumberSeven);
- 
+
 		txtNumberEight = new JTextField();
 		txtNumberEight.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberEight.setForeground(Color.BLACK);
 		txtNumberEight.setColumns(10);
 		txtNumberEight.setBackground(Color.LIGHT_GRAY);
-		txtNumberEight.setBounds(411, 202, 118, 20);
+		txtNumberEight.setBounds(563, 203, 190, 20);
 		contentPane.add(txtNumberEight);
-		
+
 		txtNumberNine = new JTextField();
 		txtNumberNine.setHorizontalAlignment(SwingConstants.CENTER);
 		txtNumberNine.setForeground(Color.BLACK);
 		txtNumberNine.setColumns(10);
 		txtNumberNine.setBackground(Color.LIGHT_GRAY);
-		txtNumberNine.setBounds(411, 232, 118, 20);
+		txtNumberNine.setBounds(563, 234, 190, 20);
 		contentPane.add(txtNumberNine);
 		
+		lblWelcherBungalowSoll = new JLabel(
+				"<html><body><center> Welcher Bungalow soll <br>angezeigt werden</center></body></html>");
+		lblWelcherBungalowSoll.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcherBungalowSoll.setForeground(Color.WHITE);
+		lblWelcherBungalowSoll.setBackground(Color.WHITE);
+		lblWelcherBungalowSoll.setBounds(553, 35, 200, 30);
+		contentPane.add(lblWelcherBungalowSoll);
+
+		btnAnzeigen = new JButton("Anzeigen");
+		btnAnzeigen.setForeground(Color.BLACK);
+		btnAnzeigen.setBackground(Color.CYAN);
+		btnAnzeigen.setBounds(602, 104, 118, 25);
+		contentPane.add(btnAnzeigen);
+		btnAnzeigen.addActionListener(this);
+		btnAnzeigen.setFocusable(false);
 		
+		btnBuchen = new JButton("Buchen");
+		btnBuchen.setForeground(Color.BLACK);
+		btnBuchen.addActionListener(this);
+		btnBuchen.setBackground(Color.CYAN);
+		btnBuchen.setBounds(110, 190, 110, 25);
+		contentPane.add(btnBuchen);
+		btnBuchen.setFocusable(false);
+
+		
+		btnBungalowListe = new JButton("Liste");
+		btnBungalowListe.setForeground(Color.BLACK);
+		btnBungalowListe.setBackground(new Color(175, 238, 238));
+		btnBungalowListe.setBounds(350, 35, 110, 25);
+		contentPane.add(btnBungalowListe);
+		btnBungalowListe.setFocusable(false);
+		
+		
+
 	}
- 
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		 /* if(Integer.parseInt(txtNumberTwo.getText()) < Integer.parseInt(txtNumberThree.getText()) && (e.getSource() == btnBuchen)) {
+			txtNumberSix.setText("Fehler!");
+			txtNumberSeven.setText("End Woche");
+			txtNumberEight.setText("früher als");
+			txtNumberNine.setText("Anfangswoche");
+			System.out.println(Integer.parseInt(txtNumberTwo.getText()) + " " + Integer.parseInt(txtNumberThree.getText()));
+		}  //Funktioniert irgendwie nicht  */
+		
+		if (e.getSource() == btnBungalowListe) {
+			Liste liste = new Liste();
+			liste.show();
+		}
+		
 		if (e.getSource() == btnBuchen) {
 			Buchungen[Integer.parseInt(txtNumberOne.getText())][0] = Integer.parseInt(txtNumberTwo.getText());
 			Buchungen[Integer.parseInt(txtNumberOne.getText())][1] = Integer.parseInt(txtNumberThree.getText());
 			Buchungen[Integer.parseInt(txtNumberOne.getText())][2] = Integer.parseInt(txtNumberFour.getText());
 			Buchungen[Integer.parseInt(txtNumberOne.getText())][3] = random.nextInt(9999);
 		}
- 
+
 		if (e.getSource() == btnAnzeigen) {
-			txtNumberSix.setText("Anfangsmonat: " + Buchungen[Integer.parseInt(txtNumberFive.getText())][0]);
-			txtNumberSeven.setText("Endemonat: " + Buchungen[Integer.parseInt(txtNumberFive.getText())][1]);
+			txtNumberSix.setText("Anfangs Woche: " + Buchungen[Integer.parseInt(txtNumberFive.getText())][0]);
+			txtNumberSeven.setText("End Woche: " + Buchungen[Integer.parseInt(txtNumberFive.getText())][1]);
 			txtNumberEight.setText("Anzahl Personen: " + Buchungen[Integer.parseInt(txtNumberFive.getText())][2]);
 			txtNumberNine.setText("Kundennummer: " + Buchungen[Integer.parseInt(txtNumberFive.getText())][3]);
 		}
